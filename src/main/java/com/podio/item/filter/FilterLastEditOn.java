655b1d3550c7a074b333b8192e911be81c81c518
Alex White <alex.white@oval.uk.com>
2021-12-17 11:24:06 +0000

    refactored last edit name to match existing filters

--- src/main/java/com/podio/item/filter/FilterLastEditOn.java
+++ src/main/java/com/podio/item/filter/FilterLastEditOn.java
@@ -6,15 +6,15 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class FilterLastEditedOn extends Filter<FilterInterval> {
+public class FilterLastEditOn extends Filter<FilterInterval> {
 
     /** non-static to allow for multi threading */
     private final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     private final FilterInterval values;
 
-    public FilterLastEditedOn(Date from, Date to) {
-        super("last_edited_on");
+    public FilterLastEditOn(Date from, Date to) {
+        super("last_edit_on");
         values = new FilterInterval();
         values.setFrom(from != null ? DATE_FORMAT.format(from) : null);
         values.setTo(to != null ? DATE_FORMAT.format(to) : null);
